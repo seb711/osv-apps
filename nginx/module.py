bf7beae984d1162c85de3cb4e16cd8d62c43aea1
Fotis Xenakis <foxen@windowslive.com>
2021-03-14 18:47:11 +0200

    nginx: make it work on read-only fs
    By default, nginx writes to various files, mainly for logging and
    caching. In order for it to work with read-only file systems, this patch
    provides an alternative nginx configuration file redirecting all these
    write paths to /tmp (which is backed by ramfs and thus is read-write).
    
    On the way, nginx is also updated to 1.19.7 (current latest).
    
    Signed-off-by: Fotis Xenakis <foxen@windowslive.com>
    Message-Id: <DB7PR03MB3771A3538F39C93616C60C7BA66D9@DB7PR03MB3771.eurprd03.prod.outlook.com>

--- nginx/module.py
+++ nginx/module.py
@@ -1,4 +1,14 @@
+import os
+
 from osv.modules import api
 
+
+ROFS = ['rofs', 'virtiofs'] # List of read-only filesystems
+
+
 api.require('libz')
-default = api.run('/nginx.so -c /nginx/conf/nginx.conf')
+
+# NOTE: This is necessary to avoid the warning about the default error log
+# location. See https://trac.nginx.org/nginx/ticket/147.
+error_log = ' -e stderr' if os.environ.get('fs_type') in ROFS else ''
+default = api.run('/nginx.so -c /nginx/conf/nginx.conf' + error_log)
