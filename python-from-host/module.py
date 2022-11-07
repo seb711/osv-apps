faf40c3dac106a1bd799beaeab2432ddf440eb1c
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2022-11-07 10:18:46 -0500

    python-from-host: replace default with the repl profile
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- python-from-host/module.py
+++ python-from-host/module.py
@@ -2,5 +2,4 @@ from osv.modules import api
 
 #For a proper interactive python terminal
 api.require('unknown-term')
-
-default = api.run(cmdline="--env=TERM=unknown /python")
+repl = api.run(cmdline="--env=TERM=unknown --env=PYTHONPATH=/ --env=PYTHONHOME=/ --env=PYTHONPLATLIBDIR=lib /python")
