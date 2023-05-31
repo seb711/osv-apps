22e1541ca18d3794053b9ca61671508a2d1944ec
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2023-05-31 11:49:10 -0400

    mysql: allow running with qemu tap device if present
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- mysql/test.sh
+++ mysql/test.sh
@@ -9,9 +9,17 @@ $THIS_DIR/../../scripts/tests/test_app_with_test_script.py \
   --start_line 'ready for connections' \
   --script_path $THIS_DIR/tester.py
 else
+TAP_DEVICE=$(ip address show qemu_tap0 | grep inet)
+if [ "$TAP_DEVICE" != "" ]; then
+$THIS_DIR/../../scripts/tests/test_app_with_test_script.py \
+  -e "$CMDLINE" \
+  --start_line 'ready for connections' \
+  --script_path $THIS_DIR/tester.py
+else
 sudo $THIS_DIR/../../scripts/tests/test_app_with_test_script.py \
   -e "$CMDLINE" \
   --vhost \
   --start_line 'ready for connections' \
   --script_path $THIS_DIR/tester.py
+fi
 fi
